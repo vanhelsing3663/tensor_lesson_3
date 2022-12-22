7c26ec9d822033a04b783c0866a87442597e47a0
Kirill Konnov <konnov360@gmail.com>
2022-12-22 11:41:15 +0300

    pep 257

--- zadanie_4_complex_numbers.py
+++ zadanie_4_complex_numbers.py
@@ -8,8 +8,8 @@ b = complex(input("Введите коэффициент b = "))
 c = complex(input("Введите коэффициент c = "))
 
 discr = b ** 2 - 4 * a * c
-print(f"Дискриминант D = {discr}")
-# На комплексной оси всегда есть корни
+print(f"Дискриминант D = {discr}") # На комплексной оси всегда есть корни
+
 x1 = (-b + (discr ** 0.5)) / (2 * a)
 x2 = (-b - (discr ** 0.5)) / (2 * a)
 print(f"x1 = ({round(x1.real, 10)}, {round(x1.imag, 10)}) \nx2 = ({round(x2.real, 10)}, {round(x2.imag, 10)})")
